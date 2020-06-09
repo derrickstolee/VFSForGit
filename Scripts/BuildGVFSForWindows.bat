a91252aba52bebc92b22c886324e360cd9fd5d54
Derrick Stolee <dstolee@microsoft.com>
2020-06-09 12:29:46 -0400

    Remove vswhere requirement
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>

--- Scripts/BuildGVFSForWindows.bat
+++ Scripts/BuildGVFSForWindows.bat
@@ -14,30 +14,7 @@ IF NOT EXIST %nuget% (
   powershell -ExecutionPolicy Bypass -Command "Invoke-WebRequest 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe' -OutFile %nuget%"
 )
 
-:: Acquire vswhere to find dev15 installations reliably.
-SET vswherever=2.6.7
-%nuget% install vswhere -Version %vswherever% || exit /b 1
-SET vswhere=%VFS_PACKAGESDIR%\vswhere.%vswherever%\tools\vswhere.exe
-
-:: Assumes default installation location for Windows 10 SDKs
-IF NOT EXIST "c:\Program Files (x86)\Windows Kits\10\Include\10.0.10240.0" (
-  echo ERROR: Could not find Windows 10 SDK Version 10240
-  exit /b 1
-)
-
-:: Use vswhere to find the latest VS installation with the msbuild component.
-:: See https://github.com/Microsoft/vswhere/wiki/Find-MSBuild
-for /f "usebackq tokens=*" %%i in (`%vswhere% -all -prerelease -latest -products * -find MSBuild\**\Bin\amd64\MSBuild.exe`) do (
- set msbuild="%%i"
-)
-
-IF NOT DEFINED msbuild (
-  echo ERROR: Could not locate a Visual Studio installation with required components.
-  echo Refer to Readme.md for a list of the required Visual Studio components.
-  exit /b 10
-)
-
-%msbuild% %VFS_SRCDIR%\GVFS.sln /p:GVFSVersion=%GVFSVersion% /p:Configuration=%SolutionConfiguration% /p:Platform=x64 || exit /b 1
+msbuild.exe %VFS_SRCDIR%\GVFS.sln /p:GVFSVersion=%GVFSVersion% /p:Configuration=%SolutionConfiguration% /p:Platform=x64 || exit /b 1
 
 dotnet publish %VFS_SRCDIR%\GVFS\FastFetch\FastFetch.csproj /p:Configuration=%Configuration% /p:Platform=x64 /p:SolutionDir=%VFS_SRCDIR%\ --runtime win-x64 --framework netcoreapp2.1 --self-contained --output %VFS_PUBLISHDIR%\FastFetch || exit /b 1
 ENDLOCAL
